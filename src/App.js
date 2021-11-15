bbfaf32803db5f1fd063e61facc31d98f23b1720
AndrewAlvaroh <andrewalvaroh@gmail.com>
2021-11-15 15:25:32 +0700

    posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stylee = {
+    textAlign : "center"
 }
 
-export default App;
+export class App extends Component {
+  state = {
+    todoList: [],
+    inputValue : ""
+  }
+  changeHandler = (e) => {
+    const {value} = e.target;
+    this.setState({
+      inputValue: value
+    })
+    
+  }
+
+  clickHandler = (e,data) => {
+    e.preventDefault()
+    this.setState({
+      todoList : [...this.state.todoList , data],
+      inputValue : ""
+    })
+  }
+  render() {
+    return (
+      <div style={stylee}>
+        <h1>Just </h1>
+        <h1>TO DO LIST </h1>
+        <hr />
+
+        <input type="text" value={this.state.inputValue} name="todoList" 
+          onChange={this.changeHandler}
+        ></input>
+        <br/>
+        <button onClick={e => this.clickHandler(e , this.state.inputValue)}>Add todoList</button>
+        
+        <br/>  <br/> 
+        <div>
+          {
+            this.state.todoList.map((list,index) => {
+                return(
+                  <div key={index + list }>
+                    {list}
+                  </div>
+                )
+            })
+          }
+        </div>
+       
+
+      </div>
+    )
+  }
+}
+
+export default App
